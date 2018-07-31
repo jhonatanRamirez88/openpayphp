84312a65b24f91bb0d747d4cec4bbbc39c0b1a4b
jonas <tonny_jho@hotmail.com>
2018-07-30 22:57:36 -0500

    Modificaciones del lado de servidor de php

---
+++ buscarreferencia.php
@@ -0,0 +1,7 @@
+<?php
+
+  echo "Variable recibida" . $_POST["numero_referencia"];
+
+  header('Location: http://localhost:8888/openpayphp/index.html');
+
+?>
--- openpaypago.php
+++ openpaypago.php
@@ -1,17 +1,24 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/vendor/autoload.php';
+	echo "Si entre aca";
+	echo "Valor de la variable: ".$_POST["token_id"];
+	echo "Valor de la variable: ".$_POST["deviceIdHiddenFieldName"];
+	echo "Valor de la variable: ".$_POST[$customer];
 	$openpay = Openpay::getInstance('mzdtln0bmtms6o3kck8f', 'sk_e568c42a6c384b7ab02cd47d2e407cab');
+	echo "Si entre aca";
+	echo $openpay;
+	echo "Si entre aca";
 	$customer = array(
-	     'name' => $_POST["name"],
-	     'last_name' => $_POST["last_name"],
-	     'phone_number' => $_POST["phone_number"],
-	     'email' => $_POST["email"],);
+	     'name' => "Juan",
+	     'last_name' => "Hernandez",
+	     'phone_number' => "7226098765",
+	     'email' => "juan@dominio.com",);
 
 	$chargeData = array(
 	    'method' => 'card',
 	    'source_id' => $_POST["token_id"],
-	    'amount' => (float)$_POST["amount"],
-	    'description' => $_POST["description"],
+	    'amount' => (float)"125.56",
+	    'description' => "Prueba de pago",
 	    //'use_card_points' => $_POST["use_card_points"], // Opcional, si estamos usando puntos
 	    'device_session_id' => $_POST["deviceIdHiddenFieldName"],
 	    'customer' => $_POST[$customer]
